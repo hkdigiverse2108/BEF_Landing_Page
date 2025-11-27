bfbccad1549518f314e96e0d36d8d416edc63484
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2025-11-27 12:54:29 +0530

    About Section UI Update

--- src/Components/Home/AboutSection.tsx
+++ src/Components/Home/AboutSection.tsx
@@ -24,30 +24,36 @@ const AboutSection = ({ aboutUs }: { aboutUs: string }) => {
     <section id="about" className=" relative overflow-hidden pt-10 pb-1 ">
       <div className="container container-p flex flex-col lg:flex-row items-center gap-12">
         {/* Left Div */}
-        <div className=" relative flex justify-center items-center w-full md:w-1/2 lg:w-full " data-aos="fade-right">
+        <div className=" relative flex max-sm:justify-start justify-center   w-[20rem] sm:w-[40rem] md:w-[48rem] lg:w-[70rem] " data-aos="fade-right">
           <div
-            className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
-              w-[300px] sm:w-[400px] md:w-[500px] h-[300px] sm:h-[400px] md:h-[500px]
-              bg-white rounded-full -z-10 shadow-md"
+            className="absolute left-1/2 sm:left-1/2 md:left-1/2 lg:1/3 top-1/2 -translate-x-1/2 -translate-y-1/2 
+              w-[300px] sm:w-[400px] md:w-[400px] h-[300px] sm:h-[400px] md:h-[400px]
+              rounded-full -z-10 shadow-md bg-white"
           ></div>
 
-          <img src={`${ImagePath}about/about-frame.png`} alt="App Main Screen" className="w-64 sm:w-80 smooth-float " data-aos="zoom-in" />
+          <img src={`${ImagePath}about/about-frame.png`} alt="App Main Screen" className="w-44 sm:w-65 smooth-float max-sm:ms-2" data-aos="zoom-in" />
           <img
             src={`${ImagePath}about/about-screen.png`}
             alt="App Secondary Screen"
-            className="w-1/2 sm:w-fit lg:w-1/2 absolute z-50  -left-0  sm:bottom-24 
-                        smooth-float-delayed"
-            data-aos="zoom-in"
+            className="  w-36  sm:w-65 md:w-70 lg:w-50 absolute z-50  right-0 sm:-right-11 md:-right-1  lg:-right-10 xl:-right-9  bottom-24   smooth-float-delayed"
+            // data-aos="zoom-in"
           />
         </div>
         {/* right Div */}
-        <div className=" w-full h-full " data-aos="fade-left">
+        <div
+          className=" w-full h-full "
+          // data-aos="fade-left"
+        >
           <div className="text-left  mb-6">
             <h2 className="text-3xl sm:text-4xl font-extrabold text-primary leading-tight mb-4">Some awesome words about app.</h2>
             <div className="text-sm text-gray-600" dangerouslySetInnerHTML={{ __html: aboutUs }} />
           </div>
 
-          <ul id="counter" className="grid grid-cols-1 sm:grid-cols-2 mt-10 justify-between gap-6" data-aos="fade-up">
+          <ul
+            id="counter"
+            className="grid grid-cols-1 sm:grid-cols-2 mt-10 justify-between gap-6"
+            //  data-aos="fade-up"
+          >
             {stats?.map((item, index) => (
               <li
                 key={index}
