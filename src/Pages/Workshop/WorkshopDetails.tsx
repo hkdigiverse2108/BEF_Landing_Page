d8c29db1f01e8913f1adb6ddc8f4a6a402e951f5
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2026-01-02 18:33:35 +0530

    workshop detail page static text added

--- src/Pages/Workshop/WorkshopDetails.tsx
+++ src/Pages/Workshop/WorkshopDetails.tsx
@@ -28,20 +28,7 @@ const WorkshopDetails = () => {
   });
   const workshop = workshopData?.data || {};
 
-  const {
-    title = "",
-    image = "",
-    pdf = "",
-    language = "",
-    totalLecture = 0,
-    totalTest = 0,
-    syllabus = "",
-    moneyBack = "",
-    description = "",
-    totalAmount = 0,
-    discountAmount = 0,
-    _id = "",
-  } = workshop;
+  const { title = "", image = "", pdf = "", language = "", totalLecture = 0, totalTest = 0, syllabus = "", moneyBack = "", description = "", totalAmount = 0, discountAmount = 0, _id = "" } = workshop;
 
   const handleChange = (_: SyntheticEvent, newValue: string) => {
     setTabIndex(newValue);
@@ -50,23 +37,13 @@ const WorkshopDetails = () => {
   if (workshopLoading) return <Loader />;
 
   return (
-    <div
-      id="Workshop"
-      className="container container-p space-y-9 py-9 bg-white rounded-xl Workshop"
-    >
+    <div id="Workshop" className="container container-p space-y-9 py-9 bg-white rounded-xl Workshop">
       <section className="group space-y-6 rounded-md relative">
         <div className="sm:hidden absolute w-full flex gap-5 justify-end px-2 pt-2 ">
           <ShareModal />
         </div>
         <figure>
-          <img
-            src={image}
-            alt={image}
-            onLoad={() => setImageLoaded(true)}
-            className={`w-full h-full  transition-opacity duration-300 rounded-lg ${
-              imageLoaded ? "opacity-100" : "opacity-0"
-            }`}
-          />
+          <img src={image} alt={image} onLoad={() => setImageLoaded(true)} className={`w-full h-full  transition-opacity duration-300 rounded-lg ${imageLoaded ? "opacity-100" : "opacity-0"}`} />
         </figure>
       </section>
       <section>
@@ -76,9 +53,7 @@ const WorkshopDetails = () => {
               <div className="bg-white border border-gray-300  w-fit h-fit px-3 py-1  rounded-md ">
                 <span>{language}</span>
               </div>
-              <div className="uppercase max-sm:text-xs text-primary font-bold ">
-                {syllabus}
-              </div>
+              <div className="uppercase max-sm:text-xs text-primary font-bold ">{syllabus}</div>
             </div>
           </section>
           <section className="flex max-sm:flex-col gap-4 justify-between">
@@ -92,25 +67,15 @@ const WorkshopDetails = () => {
           <section className="flex flex-col lg:flex-row gap-5">
             <div className="bg-card-bg  transition-all px-5 py-3 rounded w-full flex max-sm:flex-col  items-center sm:gap-5">
               <figure className="rounded-full bg-primary/10 p-3 sm:p-4 h-fit w-fit">
-                <img
-                  src={`${ImagePath}workshop/users.png`}
-                  alt="Users"
-                  className="w-8 sm:w-10 h-fit"
-                />
+                <img src={`${ImagePath}workshop/users.png`} alt="Users" className="w-8 sm:w-10 h-fit" />
               </figure>
-              <p className=" font-medium capitalize max-sm:text-sm text-center">
-                Secret Workshop on What toppers do differently
-              </p>
+              <p className=" font-medium capitalize max-sm:text-sm text-center">Secret Workshop on What toppers do differently</p>
             </div>
             <div className="bg-card-bg  transition-all px-5 py-3 rounded w-full flex flex-col sm:flex-row sm:items-center sm:gap-5">
               {/* Image + Title (top row on mobile) */}
               <div className="flex items-center gap-3 sm:mb-0">
                 <figure className="rounded-full bg-primary/10 p-3 sm:p-4 h-fit w-fit">
-                  <img
-                    src={`${ImagePath}workshop/wallet.png`}
-                    alt="Users"
-                    className="w-8 sm:w-10 h-fit"
-                  />
+                  <img src={`${ImagePath}workshop/wallet.png`} alt="Users" className="w-8 sm:w-10 h-fit" />
                 </figure>
                 <div className="space-y-2 w-full font-medium ">
                   <p>{moneyBack}</p>
@@ -141,14 +106,7 @@ const WorkshopDetails = () => {
           </Tabs>
         </div>
         <div className="mt-6">
-          {tabIndex === "about" && (
-            <CourseAboutTab
-              pdf={pdf}
-              description={description}
-              totalLecture={totalLecture}
-              totalTest={totalTest}
-            />
-          )}
+          {tabIndex === "about" && <CourseAboutTab pdf={pdf} description={description} totalLecture={totalLecture} totalTest={totalTest} />}
           {tabIndex === "lectures" && <WorkshopLecturesTab id={_id} />}
           {tabIndex === "Testimonials" && <WorkshopTestimonialsTab />}
           {tabIndex === "faqs" && <WorkshopFaqTab />}
@@ -162,17 +120,17 @@ const WorkshopDetails = () => {
             <p className="text-gray-600 font-medium">Price</p>
             <h1 className=" sm:text-xl font-bold flex gap-[2px] items-end">
               <span>₹{discountAmount}</span>
-              <span className="text-base text-red-500 font-semibold line-through decoration-2 ps-1">
-                {totalAmount}
-              </span>
+              <span className="text-base text-red-500 font-semibold line-through decoration-2 ps-1">{totalAmount}</span>
             </h1>
           </div>
-
+          {workshop.syllabus === "Workshop" && (
+            <div>
+              <p className="text-primary font-bold">Complete the workshop within 24 hours and your fee will be 100% REFUNDED.</p>
+            </div>
+          )}
           <div className=" md:w-1/4">
             <NavLink to={ROUTES.WORKSHOP.REGISTER} state={workshop}>
-              <button className="btn primary_btn !h-12 !w-full  ">
-                Join Now
-              </button>
+              <button className="btn primary_btn !h-12 !w-full  ">Join Now</button>
             </NavLink>
           </div>
         </div>
